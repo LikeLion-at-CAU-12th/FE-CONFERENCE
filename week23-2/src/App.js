7b4e089190614953bd68846290abdb0b8c49a576
jungsunbeen <tjqls041103@gmail.com>
2024-09-24 05:07:05 +0900

    feat : 랜덤 포켓몬 완성 (css 미완)

--- week23-2/src/App.js
+++ week23-2/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import RandomPokemon from "./components/RandomPokemon";
 
-function App() {
+const App = () => {
   return (
-    <>
-    </>
+    <AppContainer>
+      <Title>세상이 흉흉하니 포켓몬 하나를 데려가렴 !</Title>
+      <RandomPokemon />
+    </AppContainer>
   );
-}
+};
 
 export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+  padding: 20px;
+  background-color: #f0f0f0;
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  // font-family: 'SBAggroB';
+  // src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff') format('woff');
+  // font-weight: normal;
+  // font-style: normal;
+  font-family: 'CookieRun-Regular';
+  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/CookieRun-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+`;
--- week23-2/src/components/RandomPokemon.js
+++ week23-2/src/components/RandomPokemon.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import useFetch from "../hook/useFetch";
+
+const baseURL = "https://pokeapi.co/api/v2";
 
 const RandomPokemon = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [randomId, setRandomId] = useState(null);
+  const [fetchRange, setFetchRange] = useState([1, 1]);
 
-export default RandomPokemon
+  const handleFetchRandomPokemon = () => {
+    alert("정말 마음에 안 드시나요?");
+    const randomPokemonId = Math.floor(Math.random() * 151) + 1;
+    setFetchRange([randomPokemonId, randomPokemonId]);
+    setRandomId(randomPokemonId);
+  };
+
+  const { data, loading, error } = useFetch(baseURL, fetchRange[0], fetchRange[1]);
+
+  return (
+    <Container>
+      {loading && <LoadingText>Loading...</LoadingText>}
+      {error && <ErrorText>Error: {error.message}</ErrorText>}
+
+      {data && data.length > 0 && (
+        <div>
+          <PokemonImage
+            src={data[0].imgData.sprites.front_default}
+            alt={data[0].speciesData.name}
+          />
+          <PokemonName>{data[0].speciesData.name}</PokemonName>
+        </div>
+      )}
+       <Button onClick={handleFetchRandomPokemon}>마음에 안 들어..</Button>
+    </Container>
+  );
+};
+
+export default RandomPokemon;
+
+const Container = styled.div`
+  text-align: center;
+  padding: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 20px;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const PokemonName = styled.h2`
+  text-transform: uppercase;
+  color: #333;
+`;
+
+const PokemonImage = styled.img`
+  width: 300px;
+  height: 300px;
+`;
+
+const ErrorText = styled.p`
+  color: red;
+`;
+
+const LoadingText = styled.p`
+  color: #999;
+`;
