d80406ef8d98643756bedd6b7c1e45ba8466d8fd
joeyycho <joeycho@cau.ac.kr>
2024-10-01 12:43:26 +0900

    FEAT: 라이프사이클

--- week24/src/App.js
+++ week24/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PlusCalculator from './PlusCalculator';
+import LifeCycleExample from './LifeCycleExample';
 
 function App() {
   return (
+    <div>
   <PlusCalculator/>
+  <LifeCycleExample/>
+    </div>
   );
 }
 
